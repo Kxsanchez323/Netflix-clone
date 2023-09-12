0dbe6b79ed9c6c38d403bc06afba17357de9b4f3
Kevin Sanchez <Chilledsonic.ks2@gmail.com>
2023-09-12 17:31:20 -0400

    Brand new Repo

---
+++ src/pages/Account.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import SavedShows from '../components/SavedShows'
+
+const Account = () => {
+  return (
+    <>
+    <div className='w-full text-white'>
+    <img
+        className="absolute w-full h-[400px] object-cover"
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/855ed6e2-d9f1-4afd-90da-96023ec747c3/58126e35-739d-409f-9920-e213b5e2d640/US-en-20230828-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+        alt="/"
+      />
+      <div className='bg-black/60 fixed top-0 left-0 w-full h-[550px]'>
+        <div className='absolute top-[20%] p-4 md:p-8'>
+            <h1 className='text-3xl md:test-5xl font-bold'>My Shows</h1>
+        </div>
+      </div>
+    </div>
+    <SavedShows />
+    </>
+  )
+}
+
+export default Account
